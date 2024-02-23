80b02dba761e3444700f6c9e1426f732fd1d1c7e
Neetesh Gupta <neetesh.gupta95@gmail.com>
2024-02-23 21:48:04 +0530

    added tests to check the sum of one and two numbers

--- spec/string_calculator.rb
+++ spec/string_calculator.rb
@@ -3,6 +3,16 @@ RSpec.describe '#add' do
     expect(add('')).to be_an(Integer)
     expect(add('')).to eq(0)
   end
+
+  it 'returns the number for single digit' do
+    expect(add('1')).to eq(1)
+    expect(add('5')).to eq(5)
+  end
+
+  it 'returns the sum of two' do
+    expect(add('1,5')).to eq(6)
+    expect(add('2,3')).to eq(5)
+  end
 end
 
 # String Calculator method
