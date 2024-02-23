4ca507c087244bdec5d7aa22b521712c7bad8463
Neetesh Gupta <neetesh.gupta95@gmail.com>
2024-02-23 21:58:06 +0530

    added tests to allow the add method to handle new lines between numbers (instead of commas)

--- spec/string_calculator.rb
+++ spec/string_calculator.rb
@@ -17,6 +17,11 @@ RSpec.describe '#add' do
   it 'returns the sum of multiple numbers' do
     expect(add('3,2,1,5,6')).to eq(17)
   end
+
+  it 'returns the sum of the numbers' do
+    expect(add("1\n2,3")).to eq(6)
+    expect { add("1,\n") }.to raise_error(ArgumentError)
+  end
 end
 
 # String Calculator method
