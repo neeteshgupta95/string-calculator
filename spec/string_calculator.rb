94bb26a7a0701fde13965692eab91c37262395da
Neetesh Gupta <neetesh.gupta95@gmail.com>
2024-02-23 22:19:37 +0530

    added context block for collections of similar kind of tests

--- spec/string_calculator.rb
+++ spec/string_calculator.rb
@@ -9,17 +9,22 @@ RSpec.describe '#add' do
     expect(add('5')).to eq(5)
   end
 
-  it 'returns the sum of two numbers' do
-    expect(add('1,5')).to eq(6)
-    expect(add('2,3')).to eq(5)
+  context 'verify the sum of numbers' do
+    it 'returns the sum of two numbers' do
+      expect(add('1,5')).to eq(6)
+      expect(add('2,3')).to eq(5)
+    end
+
+    it 'returns the sum of multiple numbers' do
+      expect(add('3,2,1,5,6')).to eq(17)
+    end
+
+    it 'returns the sum of the numbers when input contains new line' do
+      expect(add("1\n2,3")).to eq(6)
+    end
   end
 
-  it 'returns the sum of multiple numbers' do
-    expect(add('3,2,1,5,6')).to eq(17)
-  end
-
-  it 'returns the sum of the numbers' do
-    expect(add("1\n2,3")).to eq(6)
+  it 'should raise error when input is not valid' do
     expect { add("1,\n") }.to raise_error(ArgumentError)
   end
 end
